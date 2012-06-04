c4f44ab9f081bc5a57762743c050f1d323bbff71
Dmitry Ledentsov <dmlled@yahoo.com>
2012-06-04 11:17:40 +0300

    Update master

--- undoredo/undoredoable.h
+++ undoredo/undoredoable.h
@@ -6,12 +6,16 @@ namespace undoable {
 struct Undoable
 {
 	virtual void Undo()=0;
+ 	virtual bool HasUndo()=0;
+	virtual void ClearUndo()=0;
 	virtual ~Undoable() {}
 };
 
 struct Redoable
 {
 	virtual void Redo()=0;
+	virtual bool HasRedo()=0;
+	virtual void ClearRedo()=0;
 	virtual ~Redoable() {}
 };
 
